115a83f6f4caeb526bbb8f8eb0b9fa61f6f86d3d
J Hubert <jhprojectsnz@gmail.com>
2023-09-18 19:36:21 +1200

    Hero appearance update

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,20 +1,29 @@
-import ContactLinks from './ContactLinks';
-import './Hero.css';
+import ContactLinks from "./ContactLinks";
+import "./Hero.css";
 
-export default function Hero(){
-    return (
-        <section className="hero-section">
-            <div className="name-container">
-                <h1>Jonathan</h1>
-                <h1>Hubert</h1>
-            </div>
-            <div className='separator-line'/> {/* separates name and text - animates in so can't use border of .intro-text */}
-            <ContactLinks />
-            <p className='intro-text'>
-                Welcome to my portfolio! <br/> I'm a passionate and driven developer with background in research science. With my expertise in web development and my problem-solving skills, I'm committed to bringing innovative solutions to every project I work on
-            </p>
-            <a className='button learn-more-btn appear-animation' href='#projects'>Learn more</a>
-        </section>
-    )
+export default function Hero() {
+  return (
+    <section className="hero-section">
+      <div className="hero-container">
+        <div className="name-container">
+          <h1>Jonathan</h1>
+          <h1>Hubert</h1>
+          <h3>Web developer</h3>
+        </div>
+        <div className="separator-line" />
+        {/* separates name and text - animates in so can't use border of .intro-text */}
+        <p className="intro-text">
+          <span className="hero-bold">Welcome to my portfolio!</span> <br /> I'm
+          a passionate and driven developer with background in research science.
+          With my expertise in web development and my problem-solving skills,
+          I'm committed to bringing innovative solutions to every project I work
+          on
+        </p>
+      </div>
+      <ContactLinks />
+      <a className="button learn-more-btn appear-animation" href="#projects">
+        Learn more
+      </a>
+    </section>
+  );
 }
-
--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -1,47 +1,66 @@
-import './ProjectCard.css';
-import { FaAngleRight } from 'react-icons/fa';
+import "./ProjectCard.css";
+import { FaAngleRight } from "react-icons/fa";
 import { motion } from "framer-motion";
 
 // Framer motion used to animate in elements as they come into view for the first time
 
-export default function ProjectCard({data}) {
-
-    return (
-        <motion.div 
-        className='project-card'
-        key={data.name}
-        initial={{
-            opacity:0,
-            x:-100,
-            }}
-        whileInView={{
-            opacity:1, 
-            x:0,
-            transition: {
-              delay: 0.2,
-              duration: 0.7
-              }
-        }}
-        viewport={{ once: true }}
-        >   
-            <div className='project-text-container'>
-                <h3>{data.name}</h3>
-                {data.discription && <p className='project-text'>{data.discription}</p>}
-                <div className='technologies'>
-                    {data.technologies.map(item => <p key={item}>{item}</p>)}
-                </div>
-            </div>
-            {data.img ?
-            <img className="project-img" src={data.img} alt={data.name}/> :
-            <div className='project-img default-img'>Coming Soon</div>
-            }
-            <div className="btn-container">
-                {data.url && <a href={data.url} target='_blank' rel="noreferrer" className='button project-btn'>Demo<FaAngleRight/></a>}
-                {data.github && <a href={data.github} target='_blank' rel="noreferrer" className='button project-btn'>GitHub<FaAngleRight/></a>}
-            </div>
-        </motion.div>
-    )
+export default function ProjectCard({ data }) {
+  return (
+    <motion.div
+      className="project-card"
+      key={data.name}
+      initial={{
+        opacity: 0,
+        x: -200,
+      }}
+      whileInView={{
+        opacity: 1,
+        x: 0,
+        transition: {
+          delay: 0.5,
+          duration: 1,
+        },
+      }}
+      viewport={{ once: true }}
+    >
+      <div className="project-text-container">
+        <h3>{data.name}</h3>
+        {data.discription && <p className="project-text">{data.discription}</p>}
+        <div className="technologies">
+          {data.technologies.map((item) => (
+            <p key={item}>{item}</p>
+          ))}
+        </div>
+      </div>
+      {data.img ? (
+        <img className="project-img" src={data.img} alt={data.name} />
+      ) : (
+        <div className="project-img default-img">Coming Soon</div>
+      )}
+      <div className="btn-container">
+        {data.url && (
+          <a
+            href={data.url}
+            target="_blank"
+            rel="noreferrer"
+            className="button project-btn"
+          >
+            Demo
+            <FaAngleRight />
+          </a>
+        )}
+        {data.github && (
+          <a
+            href={data.github}
+            target="_blank"
+            rel="noreferrer"
+            className="button project-btn"
+          >
+            GitHub
+            <FaAngleRight />
+          </a>
+        )}
+      </div>
+    </motion.div>
+  );
 }
-
-
-
